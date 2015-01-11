fce1d2e7d2700833cb031896b04ffa719b350b8d
Abhishek Lekshmanan <abhishek.lekshmanan@gmail.com>
2015-01-12 00:53:36 +0530

    Hy: fixing up imports in order to avoid conflicts

--- Hy/__init__.py
+++ Hy/__init__.py
@@ -2,4 +2,5 @@ import os
 import glob
 
 modules = glob.glob(os.path.dirname(__file__)+"/*.hy")
-__all__ = [os.path.basename(f)[:-3] for f in modules if not f.startswith('_')]
+__all__ = [os.path.basename(f)[:-3] for f in modules
+           if not f.startswith('_') and f.startswith("p")]
