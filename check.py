75ecf6bf5862d2aa3cb7eb8057f152993e5a804c
Abhishek Lekshmanan <abhishek.lekshmanan@gmail.com>
2015-01-11 19:53:00 +0530

    check solutions

---
+++ check.py
@@ -0,0 +1,19 @@
+from python import *
+
+
+def get_lines(filename):
+    with open(filename) as f:
+        yield f.readline().split()
+
+
+def get_solutions(filename):
+    return {k: v for k, v in get_lines(filename)}
+
+
+def test_solutions():
+    answers = get_solutions("solutions.txt")
+    for k, v in answers.iteritems():
+        assert str(globals()[k].solution()) == v
+
+if __name__ == "__main__":
+    check_solutions()
