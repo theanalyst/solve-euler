4dda485aa26914cf562ee76d25bb5d450cb90c8b
Abhishek Lekshmanan <abhishek.lekshmanan@gmail.com>
2015-01-24 19:23:16 +0530

    fixed a bug due to which many files weren't imported

--- Hy/__init__.py
+++ Hy/__init__.py
@@ -2,5 +2,4 @@ import os
 import glob
 
 modules = glob.glob(os.path.dirname(__file__)+"/*.hy")
-__all__ = [os.path.basename(f)[:-3] for f in modules
-           if not f.startswith('_') and f.startswith("p")]
+__all__ = [os.path.basename(f)[:-3] for f in modules]
