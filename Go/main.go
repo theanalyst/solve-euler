93ff94fdf83b4c969a886d4f2920dd88052e3f66
Abhishek Lekshmanan <abhishek.lekshmanan@gmail.com>
2015-02-04 23:47:11 +0530

    initial attempt at golang

---
+++ Go/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/theanalyst/solve_euler/p1"
+	"fmt"
+)
+
+func main(){
+	fmt.Println(p1.Solution())
+}
---
+++ Go/p1/p1.go
@@ -0,0 +1,11 @@
+package p1
+
+func Solution() int{
+	sum := 0
+	for x:=1; x <= 999; x++{
+		if x % 3 == 0 || x % 5 == 0{
+			sum += x
+		}
+	}
+	return sum
+}
